6afcb63f3672db11ec69c1ab3d00466d1a5b804e
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2025-05-08 23:03:08 +0300

    make the project files and compression function.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:data_compression/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DeltaCompression());
+}
+
+class DeltaCompression extends StatelessWidget {
+  const DeltaCompression({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Huffman Coding',
+      theme: ThemeData(brightness: Brightness.dark, useMaterial3: true),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
